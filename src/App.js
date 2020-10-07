9b656c3ad7716545d4f5534f1e11d8737d7733ed
Jason Canto <jason.canto.ext@sascar.com.br>
2020-10-06 23:36:19 -0400

    Adding new modules

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/layout/Header';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import AddProject from './components/project/AddProject'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard />
+        <Route to="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/project/NewProjectButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NewProjectButton() {
+    return (
+        <Link to="/addProject" className="btn btn-lg btn-info">
+            Create a Project
+        </Link>
+    )
+}
